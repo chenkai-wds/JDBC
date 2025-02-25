01b52ba7d4c17f46449ef2fec74146b800f5996a
Wang_ds <3276234832@qq.com>
2025-02-25 19:07:10 +0800

    EmployeeDao的实现与测试

--- src/com/atguigu/senior/util/JDBCUtilV2.java
+++ src/com/atguigu/senior/util/JDBCUtilV2.java
@@ -41,6 +41,8 @@ public class JDBCUtilV2 {
             Connection connection = threadLocal.get();
             if (connection != null) {
                 threadLocal.remove();
+                //如果开启了事务的手动提交，操作完成后，归还给连接池之前，将事务的自动提交改为true
+                connection.setAutoCommit(true);
                 connection.close();
             }
         } catch (SQLException e) {
